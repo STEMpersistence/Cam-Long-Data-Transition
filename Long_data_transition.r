70230c27af04d0630774c5340eaafe9fc26897ce
Cmotherwell <35737521+Cmotherwell@users.noreply.github.com>
2018-02-25 15:51:52 -0600

    Update Long_data_transition.r

--- Long_data_transition.r
+++ Long_data_transition.r
@@ -1,13 +1,13 @@
 library(dplyr)
 library(tidyr)
-data <- read_excel("stem_persistence_update.xlsx")
+data <- read_excel("stem_persistence_update.xlsx") # read in data
 
-data2 <- data %>% separate("graduationsemester", c("gradyear", "gradsem"), sep = 4, remove = FALSE)
-data4 <- data2 %>% separate("gradsem", c("gradsem", "gradcampus"), sep = 1, remove = TRUE)
-data3 <- data2 %>% unite("graduationsemester", c("gradyear", "gradsem"), sep = "", remove = TRUE)
+data2 <- data %>% separate("graduationsemester", c("gradyear", "gradsem"), sep = 4, remove = FALSE) # seperate columns into year and (semester and campus)
+data4 <- data2 %>% separate("gradsem", c("gradsem", "gradcampus"), sep = 1, remove = TRUE) # seperate columns into semester and campus
+data3 <- data2 %>% unite("graduationsemester", c("gradyear", "gradsem"), sep = "", remove = TRUE) # reunite year and semester
 
 
-#This will do it!!!!
+#This will reassign numeric semester values to corresponding word values
 data4$gradsem[which(data4$gradsem==1)] <- "fall"
 data4$gradsem[which(data4$gradsem==2)] <- "summer"
 data4$gradsem[which(data4$gradsem==3)] <- "spring"
